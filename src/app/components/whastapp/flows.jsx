9de3b55b3392d5936f3fdaaf1a0fddab8283a80a
David Israel Granados Elias <israeldavi0904@gmail.com>
2024-10-01 15:07:53 -0500

    actualizando la vista de flow

--- src/app/components/whastapp/flows.jsx
+++ src/app/components/whastapp/flows.jsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Button,
   Chip,
   Divider,
   FormControl,
@@ -17,10 +18,12 @@ import { actionFlow, getFlow } from "src/app/services/whatsapp/bots";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { useSnackbar } from "notistack";
 
-const FlowsContentItem = ({item, botId}) => {
-  const [chips, setChips] = useState(item.chwords.split(",") || []);
+const FlowsContentItem = ({ item, botId }) => {
+  const [chips, setChips] = useState(item.chwords?.split(",") || []);
   const [inputValue, setInputValue] = useState("");
-  const [select, setSelect] = useState(item.p_inidtype);
+  const [select, setSelect] = useState(item?.p_inidtype || "");
+  const [edit, setEdit] = useState(false);
+  const [title, setTitle] = useState(item?.chname || "");
   const { enqueueSnackbar } = useSnackbar();
   const navigate = useNavigate();
 
@@ -96,17 +99,52 @@ const FlowsContentItem = ({item, botId}) => {
     }
   };
 
+  const handleSave = async () => {
+    setEdit(false);
+    const response = await actionFlow({ ...item, chname: title }, "U");
+    if (response.codigo == 1) {
+      enqueueSnackbar(response.valor, {
+        variant: "success",
+        style: { fontSize: "1.3rem" },
+      });
+    } else {
+      enqueueSnackbar(response.valor, {
+        variant: "error",
+        style: { fontSize: "1.3rem" },
+      });
+    }
+  }
+
   return (
-    <Paper
-      className='flex flex-col flex-auto shadow rounded-2xl overflow-hidden p-10 h-max'
-    >
+    <Paper className='flex flex-col flex-auto shadow rounded-2xl overflow-hidden p-10 h-max'>
       <div className='flex items-center justify-between'>
-        <Typography
-          className='px-16 text-lg font-medium tracking-tight leading-6 truncate'
-          color='text.secondary'
-        >
-          {item.chname}
-        </Typography>
+        <div className='flex justify-between w-full truncate items-center'>
+          {edit ? (
+            <>
+              <TextField
+                fullWidth
+                size='small'
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+              <IconButton onClick={handleSave}>
+                <FuseSvgIcon size={20}>heroicons-outline:check</FuseSvgIcon>
+              </IconButton>
+            </>
+          ) : (
+            <>
+              <Typography
+                className='px-16 text-lg font-medium tracking-tight leading-6 truncate'
+                color='text.secondary'
+              >
+                {title}
+              </Typography>
+              <IconButton onClick={() => setEdit(true)}>
+                <FuseSvgIcon size={20}>heroicons-outline:pencil</FuseSvgIcon>
+              </IconButton>
+            </>
+          )}
+        </div>
         <div className='flex'>
           <IconButton onClick={() => handleFlow(item)}>
             <FuseSvgIcon size={20}>heroicons-outline:external-link</FuseSvgIcon>
@@ -123,7 +161,7 @@ const FlowsContentItem = ({item, botId}) => {
           <Select
             labelId={`role-label`}
             label='Tipos'
-            defaultValue={item.p_inidtype}
+            defaultValue={item?.p_inidtype || ""}
             onChange={(e) => {
               setSelect(e.target.value);
               handleActivate(e, item);
@@ -186,20 +224,46 @@ export const FlowsContent = () => {
   const [bot, setBot] = useState([]);
   const [searchParams] = useSearchParams();
   const botId = searchParams.get("bot");
+  const { enqueueSnackbar } = useSnackbar();
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await getFlow(botId);
-      setBot(response);
+      setBot(response.map((item) => ({ ...item, p_inidbot: botId })));
     };
     fetchData();
   }, []);
+
+  const handleAdd = async () => {
+    const response = await actionFlow({ p_inidbot: botId }, "I");
+    if (response.codigo == 1) {
+      enqueueSnackbar(response.valor, {
+        variant: "success",
+        style: { fontSize: "1.3rem" },
+      });
+      setBot([...bot, { ...response.data, p_inidbot: botId }]);
+    } else {
+      enqueueSnackbar(response.valor, {
+        variant: "error",
+        style: { fontSize: "1.3rem" },
+      });
+    }
+  };
 
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-24 w-full min-w-0'>
       {bot.map((item, index) => {
         return <FlowsContentItem item={item} botId={botId} key={index} />;
       })}
+      <Button
+        variant='contained'
+        className='w-full'
+        startIcon={<FuseSvgIcon>heroicons-outline:plus</FuseSvgIcon>}
+        onClick={handleAdd}
+      >
+        {" "}
+        Agregar{" "}
+      </Button>
     </div>
   );
 };
