9557554e9406d2fe89ef1602819f3b348efb737b
David Israel Granados Elias <israeldavi0904@gmail.com>
2024-10-01 15:21:19 -0500

    cambio en flow detalle

--- src/app/components/whastapp/flows-deatil.jsx
+++ src/app/components/whastapp/flows-deatil.jsx
@@ -59,6 +59,8 @@ export const FlowsDetailContent = () => {
   const [item, setItem] = useState(null);
   const [select, setSelect] = useState("");
   const [buttonSave, setButtonSave] = useState(true);
+  const [edit, setEdit] = useState(false);
+  const [title, setTitle] = useState("");
 
   const handleAddChip = async (event, item) => {
     if (event.key === "Enter" && inputValue.trim() !== "") {
@@ -116,9 +118,10 @@ export const FlowsDetailContent = () => {
     const fetchData = async () => {
       const response = await getFlow(botId);
       const botFind = response?.find((bot) => bot.p_inidflow === Number(flow));
-      setBot(botFind);
+      setBot({...botFind, p_inidbot: botId});
       setSelect(botFind?.p_inidtype);
       setChips(botFind?.chwords.split(","));
+      setTitle(botFind?.chname || "");
       const details = await getFlowDetail(flow);
       setDetails(
         details
@@ -267,18 +270,59 @@ export const FlowsDetailContent = () => {
     }
   };
 
+  const handleSave = async () => {
+    setEdit(false);
+    const response = await actionFlow({ ...bot, chname: title }, "U");
+    if (response.codigo == 1) {
+      enqueueSnackbar(response.valor, {
+        variant: "success",
+        style: { fontSize: "1.3rem" },
+      });
+    } else {
+      enqueueSnackbar(response.valor, {
+        variant: "error",
+        style: { fontSize: "1.3rem" },
+      });
+    }
+  }
+
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-24 w-full min-w-0 grid-rows-3 sm:grid-rows-2 md:grid-rows-1'>
       <Paper className='flex flex-col flex-auto shadow rounded-2xl overflow-hidden p-10'>
         <div className='flex flex-col justify-between h-full'>
           <div>
             <div className='flex items-center justify-between'>
-              <Typography
-                className='px-16 text-lg font-medium tracking-tight leading-6 truncate'
-                color='text.secondary'
-              >
-                {bot?.chname}
-              </Typography>
+              <div className='flex justify-between w-full truncate items-center'>
+                {edit ? (
+                  <>
+                    <TextField
+                      fullWidth
+                      size='small'
+                      value={title}
+                      onChange={(e) => setTitle(e.target.value)}
+                    />
+                    <IconButton onClick={handleSave}>
+                      <FuseSvgIcon size={20}>
+                        heroicons-outline:check
+                      </FuseSvgIcon>
+                    </IconButton>
+                  </>
+                ) : (
+                  <>
+                    <Typography
+                      className='px-16 text-lg font-medium tracking-tight leading-6 truncate'
+                      color='text.secondary'
+                    >
+                      {title}
+                    </Typography>
+                    <IconButton onClick={() => setEdit(true)}>
+                      <FuseSvgIcon size={20}>
+                        heroicons-outline:pencil
+                      </FuseSvgIcon>
+                    </IconButton>
+                  </>
+                )}
+              </div>
               <div className='flex'>
                 <IconButton onClick={() => handleDelete(bot)}>
                   <FuseSvgIcon size={20}>heroicons-outline:trash</FuseSvgIcon>
